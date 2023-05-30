3d5fc78892dc888a27bc1acd2274e17bc78314c2
Carol Pontara <111318380+carolpontara@users.noreply.github.com>
2023-05-30 13:51:23 -0300

    Add files via upload

---
+++ ByteBank/Program.cs
@@ -0,0 +1,11 @@
+﻿using bytebank.Contas;
+using bytebank.Titular;
+
+ContaCorrente conta5 = new ContaCorrente(283, "1234-X");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+ContaCorrente conta6 = new ContaCorrente(284, "9874-Z");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+ContaCorrente conta7 = new ContaCorrente(285, "1111-Z");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
